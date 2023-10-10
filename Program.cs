f3da2258c39aea8c82c17a2a0520a28c5aa7016f
Lenovo-User <Lenovo-User@DESKTOP-Q92463H>
2023-10-10 12:07:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Getters_and_Setters
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie1 = new Movie("Titatic", "Jim", "PG-13");
+            Movie movie2 = new Movie("Unreleased", "Jacky", "PG");
+
+            Console.WriteLine(movie1.Rating);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Getters_and_Setters
+{
+    class Movie
+    {
+        public string title;
+        public string director;
+        private string rating;
+
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            this.title = aTitle;
+            this.director = aDirector;
+            this.Rating = aRating;
+        }
+        public string Rating
+        {
+            get { return rating; }
+
+            set
+            {
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "R" || value == "")
+                {
+                    rating = value;
+                }
+                  else
+
+                  {
+                    rating = "NR";
+
+                  }
+            }
+        }
+    }
+
+            
+        
+    
+}
